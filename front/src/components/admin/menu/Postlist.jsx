11671662a5f292a45a0f5d8331f53113f448e8a4
Suphamethee Intharalib <suphamethee.au@gmail.com>
2025-11-26 22:24:36 +0700

    fix: correct import path for Nav component in Postlist to Deploy

--- front/src/components/admin/menu/Postlist.jsx
+++ front/src/components/admin/menu/Postlist.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Nav from "../nav";
+import Nav from "../Nav";
 // Icon
 import { CiSearch } from "react-icons/ci";
 
